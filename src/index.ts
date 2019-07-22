d9899ff2e556485f63c32eb004ee4e5fa1d50aa9
Andrew Michaud <dev@drew.life>
2019-07-21 20:07:49 -0700

    get a web app basically working, and transcribe a sample sudoku puzzle

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,134 @@
 import Vue from "vue";
+import SudokuGrid from "./components/SudokuGrid.vue";
+
+let sd = [
+    [
+        { "value": 2, "visible": true },
+        { "value": 4, "visible": true },
+        { "value": 6, "visible": false },
+
+        { "value": 8, "visible": false },
+        { "value": 5, "visible": false },
+        { "value": 7, "visible": true },
+
+        { "value": 9, "visible": false },
+        { "value": 1, "visible": false },
+        { "value": 3, "visible": false }
+    ],
+    [
+        { "value": 1, "visible": true },
+        { "value": 8, "visible": true },
+        { "value": 9, "visible": true },
+
+        { "value": 6, "visible": false },
+        { "value": 4, "visible": false },
+        { "value": 3, "visible": false },
+
+        { "value": 2, "visible": true },
+        { "value": 7, "visible": true },
+        { "value": 5, "visible": true }
+    ],
+    [
+        { "value": 5, "visible": true },
+        { "value": 7, "visible": false },
+        { "value": 3, "visible": true },
+
+        { "value": 2, "visible": false },
+        { "value": 9, "visible": false },
+        { "value": 1, "visible": true },
+
+        { "value": 4, "visible": true },
+        { "value": 8, "visible": false },
+        { "value": 6, "visible": false }
+    ],
+    [
+        { "value": 4, "visible": false },
+        { "value": 1, "visible": true },
+        { "value": 8, "visible": false },
+
+        { "value": 3, "visible": false },
+        { "value": 2, "visible": true },
+        { "value": 9, "visible": false },
+
+        { "value": 5, "visible": false },
+        { "value": 6, "visible": false },
+        { "value": 7, "visible": true }
+    ],
+    [
+        { "value": 6, "visible": false },
+        { "value": 3, "visible": false },
+        { "value": 7, "visible": true },
+
+        { "value": 4, "visible": false },
+        { "value": 8, "visible": false },
+        { "value": 5, "visible": false },
+
+        { "value": 1, "visible": false },
+        { "value": 2, "visible": true },
+        { "value": 9, "visible": false }
+    ],
+    [
+        { "value": 9, "visible": true },
+        { "value": 5, "visible": true },
+        { "value": 2, "visible": false },
+
+        { "value": 1, "visible": false },
+        { "value": 7, "visible": false },
+        { "value": 6, "visible": false },
+
+        { "value": 3, "visible": true },
+        { "value": 4, "visible": true },
+        { "value": 8, "visible": false }
+    ],
+    [
+        { "value": 7, "visible": true },
+        { "value": 6, "visible": false },
+        { "value": 4, "visible": true },
+
+        { "value": 5, "visible": false },
+        { "value": 3, "visible": false },
+        { "value": 2, "visible": true },
+
+        { "value": 8, "visible": true },
+        { "value": 9, "visible": true },
+        { "value": 1, "visible": false }
+    ],
+    [
+        { "value": 3, "visible": true },
+        { "value": 2, "visible": false },
+        { "value": 1, "visible": false },
+
+        { "value": 9, "visible": false },
+        { "value": 6, "visible": false },
+        { "value": 8, "visible": true },
+
+        { "value": 7, "visible": false },
+        { "value": 5, "visible": true },
+        { "value": 4, "visible": false }
+    ],
+    [
+        { "value": 8, "visible": true },
+        { "value": 9, "visible": false },
+        { "value": 5, "visible": true },
+
+        { "value": 7, "visible": true },
+        { "value": 1, "visible": true },
+        { "value": 4, "visible": true },
+
+        { "value": 6, "visible": true },
+        { "value": 3, "visible": false },
+        { "value": 2, "visible": false }
+    ]
+];
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
-        <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
+        <sudoku-grid :squareData="sd"/>
     </div>`,
-    data: {
-        name: "World"
+    data: { name: "World" },
+    components: {
+        SudokuGrid
     }
 });
